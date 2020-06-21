4561adcd2efde1367637ea065de4c51246e0a912
7karathrace <7karathrace@gmail.com>
2020-06-21 16:53:00 +0100

    Shrink2

--- shrink.sh
+++ shrink.sh
@@ -1,7 +1,9 @@
 mkdir -p pokemon_icons_shrunk
-for i in pokemon_icons/*.png ; do
+#for i in pokemon_icons/pokemon_icon_001*.png ; do
+for i in pokemon_icons/pokemon_icon_*.png ; do
  echo convert $i
- convert $i -fuzz 1% -trim +repage -resize '256x256' -gravity center -background transparent -extent 256x256 pokemon_icons_shrunk/$(basename $i)
+ #convert $i -fuzz 1% -trim +repage -resize '256x256' -gravity center -background transparent -extent 256x256 pokemon_icons_shrunk/$(basename $i)
+ convert $i -fuzz 1% -trim +repage pokemon_icons_shrunk/$(basename $i)
  file pokemon_icons_shrunk/$(basename $i)
 done
  
